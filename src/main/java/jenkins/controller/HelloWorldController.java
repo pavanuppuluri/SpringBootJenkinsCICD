e7a1a505c79543eec273c71ae4fe99ef80eb9e38
pavanuppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2020-06-14 10:28:15 +0530

    Modified POM & Controller
    Modified POM & Controller

--- src/main/java/jenkins/controller/HelloWorldController.java
+++ src/main/java/jenkins/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World V3";
     }
 
 }
