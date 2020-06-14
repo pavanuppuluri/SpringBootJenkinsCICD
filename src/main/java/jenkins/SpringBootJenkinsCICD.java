a88b1f184907365ce6f1f2e29ee215eca9993d58
pavanuppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2020-06-14 07:53:29 +0530

    Initial commit

---
+++ src/main/java/jenkins/SpringBootJenkinsCICD.java
@@ -0,0 +1,16 @@
+package jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootJenkinsCICD {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootJenkinsCICD.class, args);
+
+    }
+}
